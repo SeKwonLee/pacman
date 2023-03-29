7020c9861affd9fa57f6e948a497bce95839efdc
SoujanyaPonnapalli <soujanyap95@gmail.com>
2023-03-29 05:10:22 +0000

    Comment on how to compile

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Compile instructions
+# cmake .. -DIDX_PERSISTENT=OFF; make -j
+
 #Usage: ./ycsb [Num load keys] [Num run keys] [Value size] [ycsb workload type] [num threads] [num cleaners]
 
 #sudo numactl --membind=0 --cpunodebind=0 ./ycsb_flatstore 50331648 50331648 256 a 44 4 &>> ../../flatstore_results.txt
@@ -26,31 +29,33 @@
 #printf "\n" &>> ../../../viper_results.txt
 #printf "\n" &>> ../../../viper_results.txt
 
-#for workload in a b c d f
-#do
-#    for nthreads in 1 2 4 8 16 32 44
-#    do
-#        sudo numactl --membind=0 --cpunodebind=0 ./ycsb_flatstore 50331648 50331648 256 ${workload} ${nthreads} 4 &>> ../../flatstore_scale_results.txt
-#        printf "\n" &>> ../../flatstore_scale_results.txt
-#    done
-#done
-
-#for workload in a b c d f
-#do
-#    for nthreads in 1 2 4 8 16 32 44
-#    do
-#        sudo numactl --membind=0 --cpunodebind=0 ./ycsb_viper 50331648 50331648 256 ${workload} ${nthreads} 4 &>> ../../../viper_scale_results.txt
-#        printf "\n" &>> ../../../viper_scale_results.txt
-#    done
-#done
+for workload in a b c d f
+do
+    for nthreads in 44
+    do
+	sudo rm -rf /mnt/pmem0/*
+        sudo numactl --membind=0 --cpunodebind=0 ./example/ycsb_flatstore 50331648 50331648 256 ${workload} ${nthreads} 4 &>> ../results/flatstore_scale_results.txt
+        printf "\n" &>> ../results/flatstore_scale_results.txt
+    done
+done
 
 for workload in a b c d f
 do
-    for nthreads in 1 2 4 8 16 32 44
+    for nthreads in 44
+    do
+	sudo rm -rf /mnt/pmem0/*
+        sudo numactl --membind=0 --cpunodebind=0 ./benchmarks/other/ycsb_viper 50331648 50331648 256 ${workload} ${nthreads} 4 &>> ../results/viper_scale_results.txt
+        printf "\n" &>> ../results/viper_scale_results.txt
+    done
+done
+
+for workload in a b c d f
+do
+    for nthreads in 44
     do
         sudo rm -rf /mnt/pmem0/*
-        sudo numactl --membind=0 --cpunodebind=0 ./ycsb_chameleondb 50331648 50331648 256 ${workload} ${nthreads} 4 &>> ../../../chameleondb_scale_results.txt
-        printf "\n" &>> ../../../chameleondb_scale_results.txt
+        sudo numactl --membind=0 --cpunodebind=0 ./benchmarks/other/ycsb_chameleondb 50331648 50331648 256 ${workload} ${nthreads} 4 &>> ../results/chameleondb_scale_results.txt
+        printf "\n" &>> ../results/chameleondb_scale_results.txt
     done
 done
 
@@ -62,7 +67,7 @@ done
 #        printf "\n" &>> ../../flatstore_scale_1KB_results.txt
 #    done
 #done
-
+#
 #for workload in a b c d f
 #do
 #    for nthreads in 1 2 4 8 16 32 44
@@ -71,16 +76,16 @@ done
 #        printf "\n" &>> ../../../viper_scale_1KB_results.txt
 #    done
 #done
-
-for workload in a b c d f
-do
-    for nthreads in 1 2 4 8 16 32 44
-    do
-        sudo rm -rf /mnt/pmem0/*
-        sudo numactl --membind=0 --cpunodebind=0 ./ycsb_chameleondb 50331648 50331648 1024 ${workload} ${nthreads} 4 &>> ../../../chameleondb_scale_1KB_results.txt
-        printf "\n" &>> ../../../chameleondb_scale_1KB_results.txt
-    done
-done
+#
+#for workload in a b c d f
+#do
+#    for nthreads in 1 2 4 8 16 32 44
+#    do
+#        sudo rm -rf /mnt/pmem0/*
+#        sudo numactl --membind=0 --cpunodebind=0 ./ycsb_chameleondb 50331648 50331648 1024 ${workload} ${nthreads} 4 &>> ../../../chameleondb_scale_1KB_results.txt
+#        printf "\n" &>> ../../../chameleondb_scale_1KB_results.txt
+#    done
+#done
 
 #kvs_name=$1
 #for valuesize in 256 1024 4096
